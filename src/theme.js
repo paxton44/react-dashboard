d076b3c09f4028f034b17eb70292e76034150a2e
Mitchell Flood <mitchflood1@gmail.com>
2023-07-03 12:32:35 -0400

    Fixed Typography slow down and remember that .join is an array function and exists outside the array to join together two strings or objects

--- src/theme.js
+++ src/theme.js
@@ -163,34 +163,34 @@ export const themeSettings = (mode) => {
             }),
       },
             // Typography Settings
-            // typography: {
-            //     fontFamily: ["Source Sans 3", "sans-serif"].join(","),
-            //     fontSize: 12,
-            //     h1: {
-            //         fontFamily: ["Source Sans 3", "sans-serif".join(",")],
-            //         fontSize: 40,
-            //     },
-            //     h2: {
-            //         fontFamily: ["Source Sans 3", "sans-serif".join(",")],
-            //         fontSize: 32,
-            //     },
-            //     h3: {
-            //         fontFamily: ["Source Sans 3", "sans-serif".join(",")],
-            //         fontSize: 24,
-            //     },
-            //     h4: {
-            //         fontFamily: ["Source Sans 3", "sans-serif".join(",")],
-            //         fontSize: 20,
-            //     },
-            //     h5: {
-            //         fontFamily: ["Source Sans 3", "sans-serif".join(",")],
-            //         fontSize: 16,
-            //     },
-            //     h6: {
-            //         fontFamily: ["Source Sans 3", "sans-serif".join(",")],
-            //         fontSize: 14,
-            //     },
-            // },
+            typography: {
+                fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+                fontSize: 12,
+                h1: {
+                    fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+                    fontSize: 40,
+                },
+                h2: {
+                    fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+                    fontSize: 32,
+                },
+                h3: {
+                    fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+                    fontSize: 24,
+                },
+                h4: {
+                    fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+                    fontSize: 20,
+                },
+                h5: {
+                    fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+                    fontSize: 16,
+                },
+                h6: {
+                    fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+                    fontSize: 14,
+                },
+            },
     };
   };
 

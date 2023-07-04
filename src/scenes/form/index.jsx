f95eb9a3494570e23e51fad5a8f6dd0dc2769824
Mitchell Flood <mitchflood1@gmail.com>
2023-07-04 17:00:07 -0400

    form structure

--- src/scenes/form/index.jsx
+++ src/scenes/form/index.jsx
@@ -1,6 +1,6 @@
 import { Box, Button, TextField } from "@mui/material";
 import { Formik } from "formik";
-// import * as yup from "yup";
+import * as yup from "yup";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Header from "../../components/Header";
 
@@ -17,7 +17,11 @@ const Form = () => {
          subtitle="Create a New User Profile"
          />
 
-         <Formik>
+         <Formik
+         onSubmit={handleFormSubmit}
+         initialValues={initialValues}
+         validationSchema={checkoutSchema}
+         >
             {({
                 values,
                 errors,
@@ -27,16 +31,78 @@ const Form = () => {
                 handleSubmit,
             }) => (
                 <form onSubmit={handleSubmit}>
-                    <Box>
-                        <TextField />
-                        <TextField />
-                        <TextField />
-                        <TextField />
-                        <TextField />
+                    <Box
+                    display="grid"
+                    gap="30px"
+                    gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+                    sx={{
+                        "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+                      }}
+                    >
+                        <TextField
+                            fullWidth
+                            variant="filled"
+                            type="text"
+                            label="First Name"
+                            onBlur={handleBlur}
+                            value={values.firstName}
+                            name="firstName"
+                            error={!!touched.firstName && !!errors.firstName}
+                            helperText={touched.firstName && errors.firstName}
+                            sx={{ gridColum: "span 2"}}
+                        />
+                        <TextField
+                             fullWidth
+                             variant="filled"
+                             type="text"
+                             label="First Name"
+                             onBlur={handleBlur}
+                             value={values.firstName}
+                             name="firstName"
+                             error={!!touched.firstName && !!errors.firstName}
+                             helperText={touched.firstName && errors.firstName}
+                             sx={{ gridColum: "span 2"}}
+                        />
+                        <TextField
+                             fullWidth
+                             variant="filled"
+                             type="text"
+                             label="First Name"
+                             onBlur={handleBlur}
+                             value={values.firstName}
+                             name="firstName"
+                             error={!!touched.firstName && !!errors.firstName}
+                             helperText={touched.firstName && errors.firstName}
+                             sx={{ gridColum: "span 2"}}
+                        />
+                        <TextField 
+                             fullWidth
+                             variant="filled"
+                             type="text"
+                             label="First Name"
+                             onBlur={handleBlur}
+                             value={values.firstName}
+                             name="firstName"
+                             error={!!touched.firstName && !!errors.firstName}
+                             helperText={touched.firstName && errors.firstName}
+                             sx={{ gridColum: "span 2"}}
+                        />
+                        <TextField 
+                             fullWidth
+                             variant="filled"
+                             type="text"
+                             label="First Name"
+                             onBlur={handleBlur}
+                             value={values.firstName}
+                             name="firstName"
+                             error={!!touched.firstName && !!errors.firstName}
+                             helperText={touched.firstName && errors.firstName}
+                             sx={{ gridColum: "span 2"}}
+                        />
                     </Box>
                     <Box>
                         <Button>
-
+                            Create New User
                         </Button>
                     </Box>
                 
@@ -45,6 +111,32 @@ const Form = () => {
          </Formik> 
     </Box>
   );
+};
+
+const phoneRegExp = 
+/^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
+
+const checkoutSchema = yup.object().shape({
+    firstName: yup.string().required("required"),
+    lastName: yup.string().required("required"), 
+    email: yup.string().email("invalid email").required("required"),
+    contact: yup
+    .string()
+    .matches(phoneRegExp, "Phone Number Is Not Valid")
+    .required("required"),
+    address1: yup.string().required("required"),
+    address2: yup.string().required("required"),
+
+})
+
+const initialValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    contact: "",
+    address1: "",
+    address2: "",
+
 };
 
 export default Form

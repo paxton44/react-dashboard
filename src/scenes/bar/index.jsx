2396d3fa277a14c9e21c98c4848e5bfbb8f19558
Mitchell Flood <mitchflood1@gmail.com>
2023-07-05 14:36:07 -0400

    made bar chart with comp. docs were badass for setting this up

--- src/scenes/bar/index.jsx
+++ src/scenes/bar/index.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+import BarChart from "../../components/BarChart";
 
 const Bar = () => {
   return (
-    <div>Bar</div>
-  )
-}
-
+    <Box m="20px">
+      <Header title="Bar Chart" subtitle="Simple Bar Chart" />
+      <Box height="75vh">
+        <BarChart />
+      </Box>
+    </Box>
+  );
+};
 export default Bar

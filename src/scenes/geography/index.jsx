83914ed29b5a75cc934d6a39f11d01d622c1a385
Mitchell Flood <mitchflood1@gmail.com>
2023-07-05 15:00:00 -0400

    grabbed old code from prev attempt and fixed it I didnt have the right npm packs so none of the charts worked but the logic was dead on

--- src/scenes/geography/index.jsx
+++ src/scenes/geography/index.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Box, useTheme } from "@mui/material";
+import GeographyChart from "../../components/GeographyChart";
+import Header from "../../components/Header";
+import { tokens } from "../../theme";
 
 const Geography = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
   return (
-    <div>Geography</div>
-  )
-}
+    <Box m="20px">
+      <Header title="Geography" subtitle="Simple Geography Chart" />
 
-export default Geography
+      <Box
+        height="75vh"
+        border={`1px solid ${colors.grey[100]}`}
+        borderRadius="4px"
+      >
+        <GeographyChart />
+      </Box>
+    </Box>
+  );
+};
+
+export default Geography;
